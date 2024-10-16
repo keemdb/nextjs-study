6effc008452e565c1c647aa82872f27533d36639
keemdb <keemdb@gmail.com>
2024-10-16 16:29:39 +0900

    상세페이지 완성

---
+++ app/detail/[id]/page.js
@@ -0,0 +1,15 @@
+import { ObjectId } from "mongodb";
+import { connectDB } from "@/util/database";
+export default async function Detail(props) {
+
+	const db = (await connectDB).db('forum');
+	let result = await db.collection('post').findOne({_id: new ObjectId(props.params.id)});
+	console.log(props.params.id)
+	return (
+	  <div>
+		<h4>상세페이지임</h4>
+		<h4>{result.title}</h4>
+		<p>{result.content}</p>
+	  </div>
+	)
+  }
--- app/list/page.js
+++ app/list/page.js
@@ -1,6 +1,8 @@
+import { ObjectId } from "mongodb";
 import { connectDB } from "@/util/database";
+import Link from "next/link";
 
-export default async function List() {
+export default async function List(props) {
 
 	const db = (await connectDB).db('forum');
 	let result = await db.collection('post').find().toArray();
@@ -8,7 +10,7 @@ export default async function List() {
 	let list = result.map((item, idx)=>{
 		return (
 			<div className="list-item" key={idx}>
-				<h4>{item.title}</h4>
+				<h4><Link href={`detail/${item._id}`}>{item.title}</Link></h4>
 				<p>{item.content}</p>
 			</div>
 		)
